f5ec0d765ec44050f703391f1751064caa158231
Désirée <deutodesiree@gmail.com>
2018-04-22 20:27:41 +0200

    unicorn_test succeed

--- mythical-creatures/unicorn.rb
+++ mythical-creatures/unicorn.rb
@@ -1,12 +1,19 @@
 class Unicorn
-  attr_reader :name
+  attr_reader :name #getter
   attr_reader :color
-  def initialize(name)
-    @name=name
-    @color="white"
+  def initialize name, color="white"
+    @name=name  #équivaut à this.name
+    @color=color
   end
-  def white?
-    true
+  def white? #convention de nommage: function qui renvoie juste un booleen
+    if @color==="white"
+      true
+    else
+      false
+    end
+  end
+  def say string
+    "**;* #{string} **;*"
   end
 end
 
